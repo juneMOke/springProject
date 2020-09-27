971689c8ef0e179f047cb873937f5735c2d17860
junior Moke <junior.fungwamoke.etu@univ-lille.fr>
2020-09-27 20:51:04 +0200

    some modifications on template

--- src/main/java/org/o7planning/thymeleaf/controller/MainController.java
+++ src/main/java/org/o7planning/thymeleaf/controller/MainController.java
@@ -6,11 +6,11 @@ import java.util.List;
 import org.o7planning.thymeleaf.form.NumberForm;
 import org.o7planning.thymeleaf.model.NumberResult;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
@@ -18,14 +18,12 @@ public class MainController {
     private static List<NumberResult> numberResults = new ArrayList<NumberResult>();
 
     static {
-        NumberResult n1 = new NumberResult(-5);
         NumberResult n2 = new NumberResult(5);
         NumberResult n3 = new NumberResult(36);
         NumberResult n4 = new NumberResult(142);
         n2.result();
         n3.result();
         n4.result();
-        numberResults.add(n1);
         numberResults.add(n2);
         numberResults.add(n3);
         numberResults.add(n4);
@@ -33,52 +31,32 @@ public class MainController {
     }
 
     // Injectez (inject) via application.properties.
-    @Value("${welcome.message}")
-    private String message;
-
-    @Value("${error.message}")
-    private String errorMessage;
 
     @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
     public String index(Model model) {
-
-        model.addAttribute("message", message);
-
-        return "index";
-    }
-
-    @RequestMapping(value = { "/numberList" }, method = RequestMethod.GET)
-    public String numberList(Model model) {
-
-        model.addAttribute("numbers", numberResults);
-
-        return "numberList";
-    }
-
-    @RequestMapping(value = { "/registerNumber" }, method = RequestMethod.GET)
-    public String showAddPersonPage(Model model) {
-
         NumberForm numberForm = new NumberForm();
         model.addAttribute("numberForm", numberForm);
-
-        return "registerNumber";
+        model.addAttribute("numbers", numberResults);
+        return "index";
+    }
+    @GetMapping(value="/indexError")
+    public ResponseEntity<String> displayError(){
+        return new ResponseEntity<String>("BAD REQUEST ERROR", HttpStatus.BAD_REQUEST);
     }
 
-    @RequestMapping(value = { "/registerNumber" }, method = RequestMethod.POST)
-    public String savePerson(Model model, //
-                             @ModelAttribute("numberForm") NumberForm numberForm) {
 
-        int numberValue = numberForm.getNumberValue();
-        if ( numberValue > 0) {
-            NumberResult newNumberResult = new NumberResult(numberValue);
+    @RequestMapping(value = { "/registerNumber" }, method = RequestMethod.POST)
+    public String savePerson(Model model,@ModelAttribute("numberForm") NumberForm numberForm) {
+
+        if (numberForm.getNumberValue() <= -1 ) {
+            return "redirect:/indexError";
+        } else {
+            NumberResult newNumberResult = new NumberResult(numberForm.getNumberValue());
             newNumberResult.result();
             numberResults.add(newNumberResult);
-
-            return "redirect:/numberList";
+            return "redirect:/index";
         }
 
-        model.addAttribute("errorMessage", errorMessage);
-        return "registerNumber";
     }
 
 }
---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,4 @@
+$("div").map(function(div){
+        console.log("coucou");
+        div.className = "border border-secondary rounded-left"
+    })
